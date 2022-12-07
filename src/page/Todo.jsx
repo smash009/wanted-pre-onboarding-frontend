58e149d63f495b02237f00e0dc282d3d97504ec8
Nam JangHyun <smash009@naver.com>
2022-12-08 04:20:34 +0900

    Add: todo 페이지 리스트 구현

--- src/page/Todo.jsx
+++ src/page/Todo.jsx
@@ -17,9 +17,47 @@ const Todos = () => {
 
   return (
     <>
-      <p>Todos</p>
+      <div>
+        <div>
+          <h1>Todo List</h1>
+        </div>
+        <form>
+          <input placeholder="할일 입력" />
+          <button type="submit">입력</button>
+        </form>
+        <div>
+          <ul>
+            {RECOMMEND_LIST.map((list) => (
+              <li key={list.id}>
+                <div>
+                  <input type="checkbox" checked={list.isCompleted} />
+                </div>
+                <div>
+                  <span>{list.todo}</span>
+                </div>
+                <div>delete</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
 
 export default Todos;
+
+const RECOMMEND_LIST = [
+  {
+    id: 1,
+    todo: "todo2",
+    isCompleted: false,
+    userId: 1,
+  },
+  {
+    id: 2,
+    todo: "todo3",
+    isCompleted: false,
+    userId: 1,
+  },
+];
