64a4bdadb26d1b517bbcbd00d22d13851a6ea2a6
Nam JangHyun <smash009@naver.com>
2022-12-08 15:46:38 +0900

    Add: todo 페이지 게시글 작성 및 삭제 구현

--- src/page/Todo.jsx
+++ src/page/Todo.jsx
@@ -7,13 +7,67 @@ const Todos = () => {
   const navigate = useNavigate();
   const accessToken = localStorage.getItem("TOKEN");
 
+  // let myHeaders = new Headers();
+  // myHeaders.append("Authorization", `Bearer ${accessToken}`);
+
+  const [todoInputContent, setTodoInputContent] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
   useEffect(() => {
     if (accessToken) {
       navigate(`/todo`);
     } else {
       navigate(`/`);
     }
+
+    fetch(`${API_URI}todos`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${accessToken}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setTodoList(data);
+      });
   }, []);
+
+  const handleInput = (e) => {
+    setTodoInputContent(e.target.value);
+    // console.log(todoContent);
+  };
+
+  const createTodo = (e) => {
+    e.preventDefault();
+
+    fetch(`${API_URI}todos`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        todo: todoInputContent,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  const deleteTodo = (e) => {
+    e.preventDefault();
+
+    let id = e.target.dataset.index;
+    console.log(id);
+
+    fetch(`${API_URI}todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  };
 
   return (
     <>
@@ -22,20 +76,30 @@ const Todos = () => {
           <h1>Todo List</h1>
         </div>
         <form>
-          <input placeholder="할일 입력" />
-          <button type="submit">입력</button>
+          <input placeholder="할일 입력" onChange={handleInput} />
+          <button type="submit" onClick={createTodo}>
+            입력
+          </button>
         </form>
         <div>
           <ul>
-            {RECOMMEND_LIST.map((list) => (
+            {todoList?.map((list) => (
               <li key={list.id}>
                 <div>
-                  <input type="checkbox" checked={list.isCompleted} />
+                  <input type="checkbox" />
                 </div>
                 <div>
                   <span>{list.todo}</span>
                 </div>
-                <div>delete</div>
+                <div>
+                  <button
+                    type="submit"
+                    data-index={list.id}
+                    onClick={deleteTodo}
+                  >
+                    삭제
+                  </button>
+                </div>
               </li>
             ))}
           </ul>
@@ -50,13 +114,25 @@ export default Todos;
 const RECOMMEND_LIST = [
   {
     id: 1,
+    todo: "todo1",
+    isCompleted: true,
+    userId: 1,
+  },
+  {
+    id: 2,
     todo: "todo2",
     isCompleted: false,
     userId: 1,
   },
   {
-    id: 2,
+    id: 3,
     todo: "todo3",
+    isCompleted: false,
+    userId: 1,
+  },
+  {
+    id: 4,
+    todo: "todo4",
     isCompleted: false,
     userId: 1,
   },
