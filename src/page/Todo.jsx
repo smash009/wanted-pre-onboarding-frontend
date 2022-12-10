aa44bb2c02ee8017b5848f5f9661e625f54ed769
Nam JangHyun <smash009@naver.com>
2022-12-10 13:54:02 +0900

    Add: todo 페이지 업데이트 구현2

--- src/page/Todo.jsx
+++ src/page/Todo.jsx
@@ -21,25 +21,38 @@ const Todos = () => {
       navigate(`/`);
     }
 
+    // fetch(`${API_URI}todos`, {
+    //   method: "GET",
+    //   headers: { Authorization: `Bearer ${accessToken}` },
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     //setTodoList(data);
+    //     setTodoList([...data].map((list) => ({ ...list, modify: false })));
+    //     console.log(todoList);
+    //   });
+
+    fetchingApi();
+  }, []);
+
+  const fetchingApi = () => {
     fetch(`${API_URI}todos`, {
       method: "GET",
       headers: { Authorization: `Bearer ${accessToken}` },
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setTodoList(data);
+        setTodoList([...data].map((list) => ({ ...list, modify: false })));
       });
-  }, []);
+  };
 
   const handleInput = (e) => {
     setTodoInputContent(e.target.value);
-    // console.log(todoContent);
   };
 
   const handleUpdateInput = (e) => {
     setTodoUpdateContent(e.target.value);
-    // console.log(todoContent);
   };
 
   const createTodo = (e) => {
@@ -54,16 +67,14 @@ const Todos = () => {
       body: JSON.stringify({
         todo: todoInputContent,
       }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+    });
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   console.log(data);
+    // });
   };
 
   const deleteTodo = (e) => {
-    e.preventDefault();
-
     let id = e.target.dataset.index;
     console.log(id);
 
@@ -89,13 +100,33 @@ const Todos = () => {
       },
       body: JSON.stringify({
         todo: todoUpdateContent,
-        isCompleted: true,
+        isCompleted: false,
       }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+    });
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   console.log(data);
+    // });
+
+    fetchingApi();
+  };
+
+  const toggleUpdateInputBox = (e) => {
+    console.log("showUpdateInputBox", e);
+
+    setTodoList((todos) =>
+      todos.map((todo) => (todo.id === e.id ? { ...todo, modify: true } : todo))
+    );
+  };
+
+  const cancelUpdateTodo = (e) => {
+    console.log("cancelUpdateTodo", e);
+
+    setTodoList((todos) =>
+      todos.map((todo) =>
+        todo.id === e.id ? { ...todo, modify: false } : todo
+      )
+    );
   };
 
   return (
@@ -113,27 +144,49 @@ const Todos = () => {
         <div>
           <ul>
             {todoList?.map((list) => (
-              <li key={list.id}>
+              <ItemLi data-index={list.id} key={list.id}>
                 <div>
                   <input type="checkbox" />
                 </div>
-                <div>
+                <TodoListItem
+                  data-index={list.id}
+                  todoList={todoList}
+                  style={{ display: list.modify ? "none" : "inline-block" }}
+                >
                   <span>{list.todo}</span>
-                  <span>
-                    <input
-                      defaultValue={list.todo}
-                      onChange={handleUpdateInput}
-                    />
+                  <span
+                    data-index={list.id}
+                    onClick={() => {
+                      toggleUpdateInputBox(list);
+                    }}
+                  >
+                    수정
                   </span>
-                </div>
-                <div>
+                </TodoListItem>
+                <UpdateInputBox
+                  data-index={list.id}
+                  style={{ display: list.modify ? "inline-block" : "none" }}
+                >
+                  <input
+                    defaultValue={list.todo}
+                    onChange={handleUpdateInput}
+                  />
                   <button
                     type="submit"
                     data-index={list.id}
                     onClick={updateTodo}
                   >
-                    수정
+                    제출
                   </button>
+                  <button
+                    type="submit"
+                    data-index={list.id}
+                    onClick={cancelUpdateTodo}
+                  >
+                    취소
+                  </button>
+                </UpdateInputBox>
+                <div>
                   <button
                     type="submit"
                     data-index={list.id}
@@ -142,7 +195,7 @@ const Todos = () => {
                     삭제
                   </button>
                 </div>
-              </li>
+              </ItemLi>
             ))}
           </ul>
         </div>
@@ -150,6 +203,16 @@ const Todos = () => {
     </>
   );
 };
+
+const ItemLi = styled.li``;
+
+const TodoListItem = styled.div`
+  display: inline-block;
+`;
+
+const UpdateInputBox = styled.div`
+  display: none;
+`;
 
 export default Todos;
 
